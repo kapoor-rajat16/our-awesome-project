155ae1d874c98573526c9c0ab176658a855fea67
Rajat Kapoor <rajatkapoor214@gmail.com>
2023-02-11 21:49:26 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+
+connectToMongo();
+
+var cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Our Awesome Project');
+})
+
+app.listen(port, () => {
+    console.log(`Backend listening on port ${port}`);
+})
